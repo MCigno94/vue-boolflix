c8d8c305c57f1ce8c5820c91025dfdda70bc6061
MCigno94 <mattiacigno27@gmail.com>
2022-05-09 15:30:42 +0200

    added flag

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,24 @@
   <div id="app">
     <header>
     <img src="https://fontmeme.com/permalink/220509/c0312dea3f976bc738b9ef8200ecfd49.png" alt="3d-rotation" border="0">
-    <div>movies</div>
+    <div>movies & series</div>
     <input v-model="query" type="text">
     <button @click="callApi">clik</button>
     </header>
     <main>
-      <div class="container">
-        <div class="row row-cols-6">
+      <div class="container py-4">
+        <div class="row row-cols-6 py-4 g-3">
           <div class="cols" v-for="movie in movies" :key="movie.id">
-            <div class="card">
-              <img :src="movie.backdrop_path" alt="">
+            <div class="card h-100">
+              <img :src="'https://image.tmdb.org/t/p/w300' + movie.poster_path" alt="movie.title">
               <div class="card-body">
-                <h3>{{movie.title}}</h3>
-                <p>{{movie.id}}</p>
+                <h3 v-if="movie.title">{{movie.title}}</h3>
+                <h3 v-else>{{movie.name}}</h3>
+                <p v-if="movie.original_title">{{movie.original_title}}</p>
+                <p v-else>{{movie.original_name}}</p>
+                <flag  :iso ="movie.original_language" />
+                <p>Lingua: {{movie.original_language}}</p>
+                <p>Voto: {{movie.vote_average}}</p>
               </div>
             </div>
             <!-- /.card -->
@@ -38,15 +43,21 @@ export default {
   },
   data() {
     return {
+      API_URL_MOVIE: 'https://api.themoviedb.org/3/search/movie?api_key=a2c29c1744af97a151f88b2cd413254a&language=it-IT&page=1&include_adult=false&query=',
+      API_URL_SERIE: 'https://api.themoviedb.org/3/search/tv?api_key=a2c29c1744af97a151f88b2cd413254a&language=it-IT&page=1&include_adult=false&query=',
       query: '',
       movies: null,
+      language: '',
       loading: true
     }
+  },
+  computed: {
+
   },
   methods: {
     callApi(){
       axios
-      .get(`https://api.themoviedb.org/3/search/movie?api_key=a2c29c1744af97a151f88b2cd413254a&language=it-IT&page=1&include_adult=false&query=${this.query}`)
+      .get(`${this.API_URL_MOVIE}${this.query}` + `${this.API_URL_SERIE}${this.query}`)
       .then((response) => {
           this.movies = response.data.results;
           this.loading = false;
@@ -56,7 +67,8 @@ export default {
           console.log(error);
           error;
           this.error = `Sorry There is a problem! ${error}`
-      });
+      })
+      .finally(() => this.loading = false)
     },
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import FlagIcon from 'vue-flag-icon';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(FlagIcon);
+
+/* Bootstrap */
+const bootstrap = require('bootstrap');
+Vue.use(bootstrap);
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
